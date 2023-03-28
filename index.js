dbc54f8aca1c4fc5c1bee50ada859ec945e64b49
cynthia <cynthiakagz@gmail.com>
2023-03-28 20:38:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1=2
+let num2=31
+let multiply = num1*num2
+    console.log(multiply); 
+
+ let random= Math.floor(Math.random()*10) +1;
+    
+  
+
+let num3=7
+let num4=3
+let mod=num3-num4
+let subtract=num3-num4
+console.log(subtract);
+
+let max = 20
+function max() {
+  console.log(max)
+}
